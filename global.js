4e1581a7ba7f11acd09f13a18e923c820c34dfea
ɪ ɴ ʀ ʟ <inrlwabots@gmail.com>
2022-09-01 20:08:09 +0530

    ꜰɪxᴇᴅ🥵

--- global.js
+++ global.js
@@ -5,6 +5,6 @@ global._user = JSON.parse(fs.readFileSync("./lib/database/data/user.json"))
 global._group = JSON.parse(fs.readFileSync("./lib/database/data/group.json"))
 global.owner = ["917591939575"];
 global.mess = (type, m) => {
-    let msg = { wait: 'Wait, in progress', owner: 'nothing more!', premium: 'primum member!', group: 'only at grp!', private: 'Perintah ini hanya dapat digunakan di private chat!', admin: 'Perintah ini hanya dapat digunakan oleh admin group!', botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut', bot: 'desk for bot', dead: 'fiture is dead😄😂', media: 'Reply media', error: "No Results Found" }[type]
+    let msg = { wait: 'Wait, in progress', owner: 'nothing more!', premium: 'primum member!', group: 'only at grp!', private: 'only at privet chat!', admin: 'Perintah ini hanya dapat digunakan oleh admin group!', botAdmin: 'Bot bukan admin, tidak dapat mengakses fitur tersebut', bot: 'desk for bot', dead: 'fiture is dead😄😂', media: 'Reply media', error: "No Results Found" }[type]
     if (msg) return m.reply(msg, m.from, { quoted: m })
 }
